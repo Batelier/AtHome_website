c05e1cba2fe4774aa6178986566d4440b823a9d8
Batellier <baptiste.roberjot@isep.fr>
2019-04-08 16:15:45 +0200

    ''

---
+++ view/view_accueil_client.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Page maison utilisateur principal ou secondaire</title>
+	<link rel="stylesheet" type="text/css" href="css_accueil_client.css">
+</head>
+<body>
+	<!-- Insérer ici le code factorisé de l'entête et du menu --> 
+
+	<div id= "main_div">
+		<div id = "leftSide"> <!-- contient : menu et favoris -->
+			<div id="menu">
+				<ul>
+					<span id="titleMenu" class="notLastLi"> Menu </span>
+					<li class="notLastLi"><a href="">Modifier pièce</a></li>
+					<li class="notLastLi"><a href="">Ajouter pièce</a></li>
+					<li class="notLastLi"><a href="">Supprimer pièce</a></li>
+					<li><a href="">Gestion routines</a></li>
+					
+				</ul>
+			</div>
+			<div id="favoris">
+				<span id="titleMenu" class="notLastLi">Favoris</span>
+			</div>
+		</div>
+		<div id = "rightSide"> <!-- contient barre de recherche, navigation et les différentes habitations -->	
+			<div id="barreSearchNav">
+				ici barre de navigation et de recherche
+			</div>
+			<div id="listeMaison">
+				ICI LISTE DES DIFFERENTES PIECES
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Insérer ici le code factorisé du menu et du footer --> 
+</body>
