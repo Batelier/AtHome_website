cff877ef5d32a70497bafa223e03cdde08d03501
Paul-OMahony <paul.omahony@isep.fr>
2019-04-09 11:20:17 +0200

    reorganisation header paul

--- view/view_header.php
+++ view/view_header.php
@@ -1,33 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-<link href="/AtHome/view/css/css_header.css" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-<div id="header">
-		<img id="logo" src="/AtHome/res/logo_png.png" width="100px" height="100px">
-		<div id="slogan">
-			<span style="font-size: 20px"> Bienvenue sur Athome !</span>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/css_header.css" />
+	</head>
+	<body>
+	<div class="header">
+		<div class="bandeau">
+			<div class="logo">
+				<img class="logo_png" src="/AtHome/res/logo_png.png" width="100em" height="100em">
+			</div>
+			<div class="slogan">
+				<p class="slogan_txt"> Bienvenue sur AtHome !!</p>
+			</div>
+			<div class="profil">
+				<img class="profil_png" src="/AtHome/res/profil.jpg" width="100em" height="100em">
+			</div>			
 		</div>
-		<img id="touxiang" src="屏幕快照 2019-03-14 上午9.48.22.png" height="100px" width="100px">
-</div>
-	<div class="naviga">
-	<ul>
- <li><a href="#home">Home</a></li>
-  <li><a href="#compte">compte</a></li>
-  <li><a href="#consommation">consommation de l'habitat</a></li>
-  <li><a href="#aide">Aide</a></li>
-	<li><a href="#contact">contact support</a></li>
-		<li><a href="#messagerie">Messagerie</a></li>
-		<li><a href="#FAQ">FAQ</a></li>
-	<li><a href="#Forum">Forum</a></li>
+		<div class="naviguer">
+			<ul>
+				<li><a href="#home">Home</a></li>
+				<li><a href="#compte">Compte</a></li>
+				<li><a href="#consommation">Consommation de l'habitat</a></li>
+				<li><a href="#aide">Aide</a></li>
+				<li><a href="#contact">Contact support</a></li>
+				<li><a href="#messagerie">Messagerie</a></li>
+				<li><a href="#FAQ">FAQ</a></li>
+				<li><a href="#Forum">Forum</a></li>
+			</ul>
+		</div>
+	</div>	
 	
-	</ul>
-</div>
-
-
-</body>
+	</body>
 </html>
