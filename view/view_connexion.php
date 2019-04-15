35b588c65d8f921a4ce12f16aa5239c08325d36f
Batellier <baptiste.roberjot@isep.fr>
2019-04-16 01:00:10 +0200

    'rework_connexion_baptiste'

--- view/view_connexion.php
+++ view/view_connexion.php
@@ -7,26 +7,43 @@
 <body>
 	<!-- Logo , Espace texte Slogan, Se connecter, Inscription, mot de passe oublié
 			Ratings, Nouvelles, Nous contacter, Footer, fond-->
-	<div id="main_div"> <!-- main, centrage + fond-->
+	<div> <!-- main, centrage + fond-->
 		<div id ="conteneur_principal">
-			<div class="bloc_colonne1"> <!--  -->
-				<img src="res/logo_png.png" id="logo"> <img src="res/slogan.jpeg" id="slogan"> 
-				<p class="nouvelle1"> AtHome arrive bientôt chez vous</p>
-				<p class="nouvelle2"> Nouveau capteur disponible ! </p>
+			<div class="bloc_colonne1">
+				<div id="bloc_logo_slogan">
+					<div id="bloc_logo">
+						<img src="res/logo_png.png" id="logo">
+					</div>
+					<div id="bloc_slogan">
+						<span id="text_slogan">Avec AtHome, dab plus vite que ton ombre</span>
+					</div>
+				</div>
+				<div id="bloc_ratings">
+					<?php //select a random coment within those which are availables  ?>
+				</div>
+				<div id="bloc_nouvelles">
+					
+					<?php
+						//foreach news create a new bloc
+					?>
+				</div>
+				
 			</div><!--  -->
-			<div class="bloc_colonne2"> 
-				<div class="bloc_connexion">
+			<div class="bloc_colonne2">
+				<div id="bloc_connexion">
 					<form method="post" action="routeur.php" name="login">
    						<input class="input_connexion" type="mail" name="email" placeholder="Email" />
    						<input class="input_connexion" type="password" name="password" placeholder="Mot de passe" />
-   						<input class="input_connexion" type="submit" value="connexion">
+   						<input class="co_button input_connexion" type="submit" value="Se Connecter">
+					</form>
+					<form method="post" action="routeur.php?cible=controller_Page_Inscription">
+	   					<input class="co_button input_connexion" type="submit" value="Créer un compte">
 					</form>
 				</div>
-				<form method="post" action="routeur.php?cible=controller_Page_Inscription">
-   					<input class="input_compte" type="submit" value="Créer un compte">
-				</form>
+				
 				<div class="bloc_contact">
 					<form id = "Contact" method="post" action="connexion.php">
+						<br>
 						<label>Nom complet</label>
    						<input class="input_contact" type="text" name="nom_complet" placeholder="Jean Dupont" />
    						<label>Adresse mail</label>
