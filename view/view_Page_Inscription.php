d3e3d9498ac788f946c6c9770e24f8004d03991a
Batellier <baptiste.roberjot@isep.fr>
2019-04-17 20:11:43 +0200

    'maj_inscription_baptiste'

--- view/view_Page_Inscription.php
+++ view/view_Page_Inscription.php
@@ -16,41 +16,41 @@
 						<table>
 							<tr>
 								<td><label for="prenom">Prénom</label></td>
-								<td><input type="text" name="prenom" id="prenom" maxlength="20" placeholder="Jean" autofocus required
+								<td><input  class="input_field" type="text" name="prenom" id="prenom" maxlength="20" placeholder="Jean" autofocus required
 								value="<?php if(isset($prenom)) {echo($prenom);} ?>"></td>
 							</tr>
 							<tr>
 								<td><label for="nom">Nom</label></td>
-								<td><input type="text" name="nom" id="nom" maxlength="20" placeholder="Dupont" required></td>
+								<td><input class="input_field" type="text" name="nom" id="nom" maxlength="20" placeholder="Dupont" required></td>
 							</tr>
 						<tr>
 							<td><label for="email">Mail</label></td>
-							<td><input type="email" name="email" id="email" placeholder="exemple@gmail.com" required></td>
+							<td><input class="input_field" type="email" name="email" id="email" placeholder="exemple@gmail.com" required></td>
 						</tr>
 						<tr>
 							<td><label for="password">Mot de passe</label></td>
-							<td><input type="password" name="password" id="password" required></td>
+							<td><input class="input_field" type="password" name="password" id="password" required></td>
 						</tr>
 						<tr>
 							<td><label for="password_check">Vérifier mot de passe</label></td>
-							<td><input type="password" name="password_check" id="password" required></td>
+							<td><input class="input_field" type="password" name="password_check" id="password" required></td>
 						</tr>
 						<tr>
 							<td><label for="telFixe">Téléphone fixe</label></td>
-							<td><input type="tel" name="telFixe" id="telFixe" maxlength="10" required></td>
+							<td><input class="input_field" type="tel" name="telFixe" id="telFixe" maxlength="10" required></td>
 						</tr>
 						<tr>
 							<td><label for="portable">Téléphone portable</label></td>
-							<td><input type="tel" name="portable" id="portable" maxlength="10" required></td>	
+							<td><input class="input_field" type="tel" name="portable" id="portable" maxlength="10" required></td>	
 						</tr>
 						<tr>
 							<td><label for="date">Date d'inscription</label></td>
-							<td><input type="date" name="date" id="date" required></td>
+							<td><input class="input_date" type="date" name="date" id="date" required></td>
 						</tr>
 						<tr>
 							<td><label for="date">Type d'utilisateur</label></td>
 							<td>
-								<select required name='type'>
+								<select class="input_select" required name='type'>
 									<option value="utilisateur_principal">Utilisateur Principal</option>
 									<option value="gestionnaire">Gestionnaire d'immeuble</option>
 									<option value="administrateur">Administrateur</option>
---
+++ view/view_header_inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="view/css/css_header.css" />
+	</head>
+	<body>
+	<div class="header">
+		<div class="bandeau">
+			<div id="bloc_logo">
+				<a href="routeur.php"><img src="res/logo_png.png" id="logo"></a>
+			</div>
+			<div id="bloc_slogan">
+				<span id="text_slogan">Bienvenue sur AtHome !</span>
+			</div>
+			<div id="bloc_profil">
+				<img id="profil" src="/AtHome/res/profil_pic.png" width="100em" height="100em">
+			</div>			
+		</div>
+	</div>	
+	
+	</body>
+</html>
