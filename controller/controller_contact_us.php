cd317955f383e33dc53b9f56920e15b5dee531ac
ZhaoBo Hu <34899106+pigu163@users.noreply.github.com>
2019-05-10 17:11:07 +0200

    update

--- controller/controller_contact_us.php
+++ controller/controller_contact_us.php
@@ -1,18 +1,18 @@
 <?php
 require("model/model_contact_us.php");
 
-    if (isset($_POST['nom_complet']) AND isset($_POST['mail']))
-    {
+    if (isset($_POST['nom_complet']) AND isset($_POST['mail'])) {
         $nom_complet = $_POST['nom_complet'];
         $email = $_POST['mail'];
         $text = $_POST['text_area'];
-        contact_us($nom_complet,$email,$text);
+        contact_us($nom_complet, $email, $text);
         ?>
         <script>
             alert("Merci de nous contacter!")
         </script>
         <?php
         header('Location:routeur.php');
+    }
 
 
 require("view/view_connexion.php");
