6e7000e4f99972ed14ee0b086d236631312157dc
ZhaoBo Hu <34899106+pigu163@users.noreply.github.com>
2019-05-19 13:42:56 +0200

    update AJax

--- model/model_Page_gestionnaire.php
+++ model/model_Page_gestionnaire.php
@@ -1,23 +1,31 @@
 <?php
-function get_user()
-{
+$q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
+
     $con=mysqli_connect("localhost","root","","athome_db");
-    $sql="SELECT name,first_name,user_id FROM user";
+    if($_GET["q"]=="all")
+    {
+        $sql = "SELECT name,first_name,user_id FROM user";
+    }
+    else
+    {
+        $sql = "SELECT name,first_name,user_id FROM user where first_name = '".$_GET["q"]."'";
+    }
+
     $result=mysqli_query($con,$sql);
-
-
 
     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result))
         {
-            echo "<div id='customer_information'>".
+            echo "<div class='customer_information'>".
                 "<div id='num_appartement'><span>"."Appartement :n*".$row["user_id"]."</span></div>".
-                "<div id='non_prenom'><span>"."name: " . $row["name"]. "  first_name: " . $row["first_name"]."</span>".
+                "<div id='non_prenom'><span>"."name: " . $row["name"]. "  first name: " . $row["first_name"]."</span>".
                 "<div id='consommation'>"."<img src='res/2861.png_300.png' style='height: 20px'>"."</div>".
                 "<div id='deux_button'>"."<button type='button'>Contact</button>"."<button type='button'>Alert Consommation</button>"."</div>".
                 "</div>";
         }
-    } else {
+    } else
+        {
         echo "0 result";
     }
-}
+
+    mysqli_close($con);
--- view/view_accueil_gestionnaire.php
+++ view/view_accueil_gestionnaire.php
@@ -4,7 +4,39 @@
     <meta charset="UTF-8">
     <title>accueil_gestionnaire</title>
     <link href="view/css/css_accueil_gestionnaire.css" rel="stylesheet" type="text/css">
+    <script>
+        function show(str)
+        {
 
+            if (str=="")
+            {
+                document.getElementById("txtHint").innerHTML="";
+                return;
+            }
+            if (window.XMLHttpRequest)
+            {
+                // IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            }
+            else
+            {
+                // IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function()
+            {
+
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                {
+                    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+                }
+            }
+
+            xmlhttp.open("GET","model/model_Page_gestionnaire.php?q="+str,true);
+            xmlhttp.send();
+
+        }
+    </script>
 </head>
 <body>
 <div id ="conteneur_principal">
@@ -32,12 +64,22 @@
             </form>
     </div>
     </div>
+
     <div id="container_customer_information">
-        <?php
-        require("model/model_Page_gestionnaire.php");
-        get_user();
-        ?>
-    </div>
+        <div id="choix">
+            <form id="form_choix">
+                <script>
+                    function f1(chk){
+                        var t = document.getElementById("text_first_name");
+                        t.disabled = !chk.checked;
+                    }
+                </script>
+                all<input type="radio" name="select" value="all" onchange="show(this.value)"><br>
+                first name:<input type="radio" name="select" onclick="f1(this)">
+                <input type="text" name="first_name" id="text_first_name" disabled="disabled" onchange="show(this.value)">
+            </form>
+        </div>
+        <div id="txtHint"></div>
 </div>
 </div>
 
