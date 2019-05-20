f4679cc0d39b2c5765cf89afd2833fb1b2bf9979
ZhaoBo Hu <34899106+pigu163@users.noreply.github.com>
2019-05-20 15:24:35 +0200

    update

--- model/model_Page_gestionnaire.php
+++ model/model_Page_gestionnaire.php
@@ -1,6 +1,5 @@
 <?php
 $q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
-
     $con=mysqli_connect("localhost","root","","athome_db");
     if($_GET["q"]=="all")
     {
@@ -17,10 +16,10 @@ $q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
         while($row = mysqli_fetch_assoc($result))
         {
             echo "<div class='customer_information'>".
-                "<div id='num_appartement'><span>"."Appartement :n*".$row["user_id"]."</span></div>".
-                "<div id='non_prenom'><span>"."name: " . $row["name"]. "  first name: " . $row["first_name"]."</span>".
-                "<div id='consommation'>"."<img src='res/2861.png_300.png' style='height: 20px'>"."</div>".
-                "<div id='deux_button'>"."<button type='button'>Contact</button>"."<button type='button'>Alert Consommation</button>"."</div>".
+                "<div class='num_appartement'></div><span>"."Appartement :n*".$row["user_id"]."</span></div>".
+                "<div class='non_prenom'><span>"."name: " . $row["name"]. "  first name: " . $row["first_name"]."</span>".
+                "<div class='consommation'>"."<img src='res/2861.png_300.png' style='height: 20px'>"."</div>".
+                "<div class='deux_button'>"."<button type='button'>Contact</button>"."<button type='button'>Alert Consommation</button>"."</div>".
                 "</div>";
         }
     } else
