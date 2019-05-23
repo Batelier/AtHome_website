24c1a337ae073ef080409cade9c315ebe6273003
ZhaoBo Hu <34899106+pigu163@users.noreply.github.com>
2019-05-23 11:16:32 +0200

    update image

--- model/model_Page_Inscription.php
+++ model/model_Page_Inscription.php
@@ -8,6 +8,35 @@ function register($name, $first_name, $mail, $password, $phone, $mobile, $regist
 	global $db;
 	$req = $db-> prepare('INSERT INTO user(name, first_name, password, mail, phone, mobile, registration, user_type) VALUES (?, ?,?,?,?,?,?,?)');
 	$req-> execute([$name,$first_name,$password,$mail,$phone,$mobile,$registration,$user_type]);
+
 }
+
+function getUserId($name,$first_name,$mail)
+{
+    db_connect();
+    //enregistrement de la personne dans la database
+    global $db;
+    $req = $db-> prepare('SELECT user_id from user where name=? and first_name=? and mail=?');
+    $req->execute([$name,$first_name,$mail]);
+    $values = $req -> fetch();
+    return $values["user_id"];
+}
+
+function register_image($name,$first_name,$mail)
+{
+        db_connect();
+        //enregistrement de la personne dans la database
+        global $db;
+        $form_data_name = $_FILES['image']['name'];
+        $form_data_size = $_FILES['image']['size'];
+        $form_data_type = $_FILES['image']['type'];
+        $form_data = $_FILES['image']['tmp_name'];
+        $data = addslashes(fread(fopen($form_data, "r"), filesize($form_data)));
+        $req = $db->prepare('INSERT INTO image(bin_data,filename,filesize,filetype,user_id) VALUES (?,?,?,?,?)');
+        $req->execute([$data, $form_data_name, $form_data_size, $form_data_type, getUserId($name, $first_name, $mail)]);
+
+}
+
+
 
 
--- model/model_Page_gestionnaire.php
+++ model/model_Page_gestionnaire.php
@@ -3,20 +3,25 @@ $q = isset($_GET["q"]) ? intval($_GET["q"]) : '';
     $con=mysqli_connect("localhost","root","","athome_db");
     if($_GET["q"]=="all")
     {
-        $sql = "SELECT name,first_name,user_id FROM user";
+        $sql = "SELECT user.name,user.first_name,user.user_id,image.bin_data,image.filetype FROM user inner join image on user.user_id = image.user_id";
     }
     else
     {
-        $sql = "SELECT name,first_name,user_id FROM user where first_name = '".$_GET["q"]."'";
+        $sql = "SELECT user.name,user.first_name,user.user_id,image.bin_data,image.filetype FROM user inner join image on user.user_id = image.user_id where first_name = '".$_GET["q"]."'";
     }
 
     $result=mysqli_query($con,$sql);
 
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0)
+    {
         while($row = mysqli_fetch_assoc($result))
         {
+           $data = $row["bin_data"];
+           $type = $row["filetype"];
+           Header( "Content-type:$type");
             echo "<div class='customer_information'>".
                 "<div class='num_appartement'></div><span>"."Appartement :n*".$row["user_id"]."</span></div>".
+                "<div class='photo'>". $data."</div>".
                 "<div class='non_prenom'><span>"."name: " . $row["name"]. "  first name: " . $row["first_name"]."</span>".
                 "<div class='consommation'>"."<img src='res/2861.png_300.png' style='height: 20px'>"."</div>".
                 "<div class='deux_button'>"."<button type='button'>Contact</button>"."<button type='button'>Alert Consommation</button>"."</div>".
